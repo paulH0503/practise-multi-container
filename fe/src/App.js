26fe13dce4d9a718f987bb108e8cf66d12e8b329
nguyen duc luong phat <nguyenducluongphat7049@gmail.com>
2020-09-28 02:34:48 +0700

    update fe source and travis

---
+++ fe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Fib />
+  );
+}
+
+export default App;
